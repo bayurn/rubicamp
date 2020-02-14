b229a377d15e9093ecc9f3a76cc2d66dbe703cca
bayurn <bayurizkynurseto19@gmail.com>
2020-02-14 22:54:57 +0700

    Challenge 4 is updated

--- 1_logic-challenge/challenge4.js
+++ 1_logic-challenge/challenge4.js
@@ -1,18 +1,9 @@
-// function checkPrime(n) {
-//     for (var a = 2; a < n; a++) {
-//         if (n % a === 0) {
-//             return false;
-//         }
-//     }
-//     return true;
-// }
-
 function indexPrime(param1) {
     let n = 2;
     let i = 0;
     while (i < param1) {
         let prime = true;
-        for (let a = 2; a < n; a++)
+        for (let a = 2; a <= Math.sqrt(n); a++)
             if (n % a === 0) prime = false;
         if (prime) {
             i++;
