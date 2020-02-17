0ae0159e70b7b19577ca11266f71540b05775cb1
bayurn <bayurizkynurseto19@gmail.com>
2020-02-17 16:01:42 +0700

    Feature filter done

--- 6_web/challenge20/server.js
+++ 6_web/challenge20/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const fs = require('fs');
 const sqlite3 = require('sqlite3').verbose();
 
 let db = new sqlite3.Database("database.db", (err) => {
@@ -16,38 +17,68 @@ app.set("views", path.join(__dirname, "views")); // specify the views directory
 app.set("view engine", "ejs");
 
 app.use("/", express.static(path.join(__dirname, "public")));
-
+// db.serialize(() => {
+//     let sqlgetData = `SELECT * FROM bread;`
+//     db.all(sqlgetData, (err, row) => {
+//         if (err) throw err
+//         res.render("index", { data: row });
+//     })
+// })
 app.get("/", (req, res) => {
-    // db.serialize(() => {
-    //     let sqlgetData = `SELECT * FROM bread;`
-    //     db.all(sqlgetData, (err, row) => {
-    //         if (err) throw err
-    //         res.render("index", { data: row });
-    //     })
-    // })
     let result = [];
     let dataFilter = false;
-    
+    let check = req.query;
+    let sql = `SELECT * FROM bread`
+
+    console.log(check)
+
     if (req.query.check_id && req.query.id) {
-        result.push(`id = ${req.query.id}`)
+        console.log('masuk id');
+        result.push(`id = ${parseInt(req.query.id)}`)
+        dataFilter = true;
     }
+
     if (req.query.check_string && req.query.string) {
-        result.push(`string = ${req.query.string}`)
+        result.push(`string = '${req.query.string}'`)
+        dataFilter = true;
     }
+
     if (req.query.check_integer && req.query.integer) {
-        result.push(`integer = ${req.query.integer}`)
+        result.push(`integer = ${parseInt(req.query.integer)}`)
+        dataFilter = true;
     }
+
     if (req.query.check_float && req.query.float) {
-        result.push(`float = ${req.query.float}`)
+        result.push(`float = '${req.query.float}'`)
+        dataFilter = true;
     }
-    if (req.query.check_date && result.query.date) {
-        result.push(`date = ${req.query.date}`)
+
+    if (req.query.check_date && req.query.startDate && req.query.endDate) {
+        console.log(req.query.endDate);
+        result.push(`date BETWEEN ${req.query.startDate} AND ${req.query.endDate}`)
+        dataFilter = true;
     }
-    if (req.query.check_boolean && req.query.boolean) {
-        result.push(`boolean = ${req.query.boolean}`)
+
+    if (req.query.check_boolean && check.boolean) {
+        result.push(`boolean = '${req.query.boolean}'`)
+        dataFilter = true;
     }
+
+    if(dataFilter == true){
+        sql += ` WHERE ${result.join(' AND ')}`
+        console.log(sql);
+    }
+
+    console.log(sql)
+    db.all(sql, (err, row) => {
+        if (err) throw err;
+        res.render("index", {
+            data: row,
+            query: req.query
+        });
+    });
 });
-    
+
 app.get("/add", (req, res) => {
     const id = req.params.id;
     res.render("add");
@@ -70,7 +101,7 @@ app.post("/add", (req, res) => {
     db.serialize(() => {
         let sqladdData = `INSERT INTO bread (string, integer, float, date, boolean)
         VALUES ('${string}', '${integer}', '${float}', '${date}', '${boolean}')`
-        db.run(sqladdData, (err, row) => {
+        db.run(sqladdData,(err, row) => {
             if (err) throw err;
             res.redirect("/");
         });
@@ -84,7 +115,6 @@ app.post("/edit/:id", (req, res) => {
     let sqleditData = `UPDATE bread SET string = '${string}', integer = '${integer}', float = '${float}', date = '${date}', boolean = '${boolean}'
         WHERE id = '${id}';`
     console.log(sqleditData);
-
 
     db.run(sqleditData, err => {
         if (err) {
@@ -106,6 +136,6 @@ app.get("/delete/:id", (req, res) => {
     })
 })
 
-app.listen(3002, () => {
-    console.log(`web ini berjalan di port 3000!`);
+app.listen(3001, () => {
+    console.log(`web ini berjalan di port 3001!`);
 });
