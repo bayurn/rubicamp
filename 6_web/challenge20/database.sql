82383be926b8cc95798d3afa40b596725f2996ad
bayurn <bayurizkynurseto19@gmail.com>
2020-02-17 09:28:14 +0700

    Update database.sql

--- 6_web/challenge20/database.sql
+++ 6_web/challenge20/database.sql
@@ -1,5 +1,5 @@
 CREATE TABLE bread (
-    ID      INTEGER     PRIMARY KEY     AUTOINCREMENT,
+    id      INTEGER     PRIMARY KEY     AUTOINCREMENT,
     integer INTEGER,
     string  TEXT,
     float   FLOAT,
@@ -9,7 +9,10 @@ CREATE TABLE bread (
 
 INSERT INTO bread (string, integer, float, date, boolean)
 VALUES ('Mumu', 121, 79.90, 2017-03-23, 'true'),
-('Mimi', 122, 99.40, 2019, 'false'),
-('Momo', 123, 79.90, 2017-03-23, 'true');
+('Mimi', 122, 99.40, 2019-09-13, 'false'),
+('Momo', 123, 79.90, 2017-07-16, 'true');
 
-DELETE * FROM bread;
+SELECT * FROM bread WHERE id = 2 
+AND string = 'Mimi' AND integer = 122 AND 
+float = '99.40' AND boolean = 'false' 
+AND date BETWEEN 1991 AND 1997;
