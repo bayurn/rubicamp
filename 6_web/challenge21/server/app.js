ca2e6da57912d67fa67a88150e5200d6d35d615f
bayurn <bayurizkynurseto19@gmail.com>
2020-02-19 21:59:36 +0700

    Add app.js

---
+++ 6_web/challenge21/server/app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+const { Pool } = require('pg')
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'bread',
+    password: '1234',
+    port: 5432
+})
+
+var indexRouter = require('./routes/index')(pool);
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api', indexRouter);
+
+module.exports = app;
